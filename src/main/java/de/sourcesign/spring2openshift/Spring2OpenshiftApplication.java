66191eaad50a0cc370331d5e42726d57cd921090
LD1024 <Lars@10.227.249.107>
2018-12-04 12:22:09 +0100

    Initial Commit

---
+++ src/main/java/de/sourcesign/spring2openshift/Spring2OpenshiftApplication.java
@@ -0,0 +1,12 @@
+package de.sourcesign.spring2openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2OpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2OpenshiftApplication.class, args);
+	}
+}
---
+++ src/test/java/de/sourcesign/spring2openshift/Spring2OpenshiftApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sourcesign.spring2openshift;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring2OpenshiftApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
